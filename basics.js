62082d7d20fcdf2b66a55d10c3edb9a5e7d09ff5
marufropy <marufropy@gmail.com>
2020-08-15 18:36:28 +0600

    template string

--- basics.js
+++ basics.js
@@ -9,11 +9,11 @@ function addNum(a, b = 0){
 const result = addNum(15);
 console.log(result);
 
-// const firstName = "Maruf"
-// const lastName = "Ropy"
-// const fullName =`${firstName} ${lastName}`;
-// console.log(fullName);
+const firstName = "Maruf"
+const lastName = "Ropy"
+const fullName =`${firstName} ${lastName}`;
+console.log(fullName);
 
-// const doubleIt = (num) => num * 2;
-// const output = doubleIt(5);
-// console.log(output);
+const doubleIt = (num) => num * 2;
+const output = doubleIt(5);
+console.log(output);
